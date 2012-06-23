69bfe65619263716470f094339ddc32daee6b1af
Bryan <bryan@inventory-sys.com>
2012-06-23 21:51:53 +0800

    beautified

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include("includes/connect.php");
 include("includes/html_codes.php");
 ?>
 
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -17,10 +16,10 @@ include("includes/html_codes.php");
 </head>
 
 <body>
-	<div id="wrapper">
-    	<?php mainHeader();  ?>  
-        	<div id="top_search">
-          <?php 
+<div id="wrapper">
+<?php mainHeader();  ?>
+<div id="top_search">
+  <?php 
 		 if( isset($_SESSION['username']) ) {
 			 mainAnouncements();
 			 }else{
@@ -28,21 +27,17 @@ include("includes/html_codes.php");
 				 }	  
 		  
 		   ?>
-           </div>
+</div>
 <div id="main_section">
-
-	<?php if( isset($_SESSION['username']) ) {
+  <?php if( isset($_SESSION['username']) ) {
 		echo '<br/><br/><br/><br/><br/><h3>Inventory Portal</h3>';
 		echo '<h3><a href="../inventory/employee/home.php" class="bigbutton">HOME</a></h3>';
 		}
 	else{
 		 echo '<br/><br/><br/><br/><br/><br/><br/><h3><a href="login.php" class="bigbutton">LOGIN</a></h3>';}?>
-                
-      		</div>
-            
-      		<div id="main_footer" class="links">
-            <?php mainFooter(); ?>	
-    </div>
-    
+</div>
+<div id="main_footer" class="links">
+  <?php mainFooter(); ?>
+</div>
 </body>
 </html>
